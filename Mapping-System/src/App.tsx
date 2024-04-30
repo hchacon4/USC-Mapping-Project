9569e0d526f3859822f5c3f504c514f48375f7e8
Brian Trang <btrang@assessor.lacounty.gov>
2024-04-30 13:21:24 -0700

    sandboxing wmts

--- Mapping-System/src/App.tsx
+++ Mapping-System/src/App.tsx
@@ -479,6 +479,113 @@ function EsriMap({  }) {
       // the following code is based on this sample:
       // https://developers.arcgis.com/javascript/latest/sample-code/webmap-basic/index.html
       // first lazy-load the esri classes
+      const lods = [
+        {
+            level: 0,
+            resolution: 156543.03392800014,
+            scale: 5.91657527591555e8
+        },
+        {
+            level: 1,
+            resolution: 78271.5169639999,
+            scale: 295828763.795777
+        },
+        {
+            level: 2,
+            resolution: 39135.7584820001,
+            scale: 147914381.897889
+        },
+        {
+            level: 3,
+            resolution: 19567.8792409999,
+            scale: 73957190.948944
+        },
+        {
+            level: 4,
+            resolution: 9783.93962049996,
+            scale: 36978595.474472
+        },
+        {
+            level: 5,
+            resolution: 4891.96981024998,
+            scale: 18489297.737236
+        },
+        {
+            level: 6,
+            resolution: 2445.98490512499,
+            scale: 9244648.868618
+        },
+        {
+            level: 7,
+            resolution: 1222.99245256249,
+            scale: 4622324.434309
+        },
+        {
+            level: 8,
+            resolution: 611.49622628138,
+            scale: 2311162.217155
+        },
+        {
+            level: 9,
+            resolution: 305.748113140558,
+            scale: 1155581.108577
+        },
+        {
+            level: 10,
+            resolution: 152.874056570411,
+            scale: 577790.554289
+        },
+        {
+            level: 11,
+            resolution: 76.4370282850732,
+            scale: 288895.277144
+        },
+        {
+            level: 12,
+            resolution: 38.2185141425366,
+            scale: 144447.638572
+        },
+        {
+            level: 13,
+            resolution: 19.1092570712683,
+            scale: 72223.819286
+        },
+        {
+            level: 14,
+            resolution: 9.55462853563415,
+            scale: 36111.909643
+        },
+        {
+            level: 15,
+            resolution: 4.77731426794937,
+            scale: 18055.954822
+        },
+        {
+            level: 16,
+            resolution: 2.38865713397468,
+            scale: 9027.977411
+        },
+        {
+            level: 17,
+            resolution: 1.19432856685505,
+            scale: 4513.988705
+        },
+        {
+            level: 18,
+            resolution: 0.59716428337097172,
+            scale: 2256.9943526
+        },
+        {
+            level: 19,
+            resolution: 0.2985821416854858,
+            scale: 1128.497176344
+        },
+        {
+            level: 20,
+            resolution: 0.1492910708694458,
+            scale: 564.248588172
+        }
+    ];
       loadModules([        "esri/Map",
       "esri/Basemap",
       "esri/views/MapView",
@@ -492,7 +599,7 @@ function EsriMap({  }) {
             new WMTSLayer({
               url:"https://svc.pictometry.com/Image/BCC27E3E-766E-CE0B-7D11-AA4760AC43ED/wmts",
               activeLayer: { id:"PICT-LARIAC5--u6URKu1Fx3", tileMatrixSetId: "GoogleMapsCompatible" },
-              serviceMode: "KVP",
+              corsEnabled:"svc.pictometry.com"
             })
           ]
         });
@@ -501,21 +608,57 @@ function EsriMap({  }) {
           baseLayers: [
             new WMTSLayer({
               url: "https://svc.pictometry.com/Image/BCC27E3E-766E-CE0B-7D11-AA4760AC43ED/wmts",
-              activeLayer: { id: "PICT-LARIAC5--u6URKu1Fx3", tileMatrixSetId:"GoogleMapsCompatible" },
+              activeLayer: { id: "PICT-LARIAC5--SxmDvXHvYJ", tileMatrixSetId:"GoogleMapsCompatible" },
               corsEnabled:"svc.pictometry.com"
             })
           ]
         });
 
         const map = new Map({
-          basemap: plainIGNBasemap
+          basemap: plainIGNBasemap,
+          center: [-118.648, 34.07314], 
+          // center: {
+          //   type: "point", 
+          //   x: 699, 
+          //   y: 1633,
+          //   spatialReference: {
+          //     wkid: 102100,
+          //     latestWkid: 3857
+          // },
+          // },
+          // //zoom doesn't work because no LOD
+          zoom:10,
+          constraints:{
+            lods:lods
+          },
+          spatialReference: {
+            wkid: 102100,
+            latestWkid: 3857
+        },
         });
 
         const view = new MapView({
           container: mapEl.current,
           map: map,
-          center: [-118.2417, 34.0541],
-
+          center: [-118.648, 34.07314], 
+          // center: {
+          //   type: "point", 
+          //   x: 699, 
+          //   y: 1633,
+          //   spatialReference: {
+          //     wkid: 102100,
+          //     latestWkid: 3857
+          // },
+          // },
+          // //zoom doesn't work because no LOD
+          zoom:10,
+          constraints:{
+            lods:lods
+          },
+          spatialReference: {
+            wkid: 102100,
+            latestWkid: 3857
+        },
         });
 
         view.ui.add(
